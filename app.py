acda9b14a2c7e2c009f1ed48712d655824eb7b62
Harsha Vardhan <harsha6084@gmail.com>
2024-02-09 16:47:17 -0600

    Initial Commit.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import traceback
 import random
 import sqlite3
 from datetime import datetime
-from flask import * # Flask, g, redirect, render_template, request, url_for
+from flask import * # Flask, g, redirect, render_template, request, url_for, session
 from functools import wraps
 
 app = Flask(__name__)
@@ -75,11 +75,19 @@ def render_with_error_handling(template, **kwargs):
         t = traceback.format_exc()
         return render_template('error.html', args={"trace": t}), 500
 
+def validate_user_api_key(req):
+    user_id = req.headers['User-Id']
+    api_key = req.headers['Api-Key']
+    if user_id and api_key:
+        return query_db('select * from users where id = ? and api_key = ?', [user_id, api_key], one=True)
+    return None
+
 # ------------------------------ NORMAL PAGE ROUTES ----------------------------------
 
 @app.route('/')
 def index():
-    print("index") # For debugging
+    print('request.headers start:', request.headers) # For debugging
+    print('request.headers end')  # For debugging
     user = get_user_from_cookie(request)
 
     if user:
@@ -143,14 +151,14 @@ def login():
         return redirect('/')
     
     if request.method == 'POST':
-        name = request.form['name']
-        password = request.form['name']
+        name = request.form['username']
+        password = request.form['password']
         u = query_db('select * from users where name = ? and password = ?', [name, password], one=True)
-        if user:
+        if u['name'] == name:
             resp = make_response(redirect("/"))
-            resp.set_cookie('user_id', u.id)
-            resp.set_cookie('user_password', u.password)
-            return resp
+            resp.set_cookie('user_id', u['name'])
+            resp.set_cookie('user_password', u['password'])
+            return render_with_error_handling('index.html', failed=False)  
 
     return render_with_error_handling('login.html', failed=True)   
 
@@ -161,26 +169,100 @@ def logout():
     resp.set_cookie('user_password', '')
     return resp
 
+def get_max_msg_id():
+    return query_db('select max(id) from messages', [], one=True)
+
 @app.route('/rooms/<int:room_id>')
 def room(room_id):
     user = get_user_from_cookie(request)
     if user is None: return redirect('/')
-
+    
     room = query_db('select * from rooms where id = ?', [room_id], one=True)
-    return render_with_error_handling('room.html',
-            room=room, user=user)
+    return render_with_error_handling('room.html', room=room, user=user)
 
 # -------------------------------- API ROUTES ----------------------------------
 
 # POST to change the user's name
-@app.route('/api/user/name')
+@app.route('/api/update/username', methods=['POST'])
 def update_username():
-    return {}, 403
+    user = validate_user_api_key(request)
+    if not user:
+        return render_template('error.html', args={"trace: ": 'Invalid API Key'}), 401
+    
+    if request.method == 'POST':
+        temp = query_db('update users set name = ? where api_key = ? returning id, name',
+            (request.args['user_name'], request.headers['Api-Key']),
+            one=True
+        )
+        # resp = make_response(redirect('/'))
+        # resp.set_cookie('user_id', request.args['user_name'])
+        resp = make_response(redirect('/profile'))
+        resp.set_cookie('user_id', request.args['user_name'])
+        return {}
+    return {'Status': 'Failed for Unknown Reasons'}, 403
 
 # POST to change the user's password
+@app.route('/api/update/password', methods=['POST'])
+def update_password():
+    user = validate_user_api_key(request)
+    if not user:
+        return render_template('error.html', args={"trace: ": 'Invalid API Key'}), 401
+    
+    if request.method == 'POST':
+        temp = query_db('update users set password = ? where api_key = ? returning id, name',
+            (request.headers['Password'], request.headers['Api-Key']),
+            one=True
+        )
+        # resp = make_response(redirect('/'))
+        # resp.set_cookie('user_password', request.headers['Password'])
+        resp = make_response(redirect('/profile'))
+        resp.set_cookie('user_password', request.headers['Password'])
+        return {}
+    return {'Status': 'Failed for Unknown Reasons'}, 403
 
 # POST to change the name of a room
+@app.route('/api/update/room', methods=['POST'])
+def update_room():
+    user = validate_user_api_key(request)
+    if not user:
+        return render_template('error.html', args={"trace: ": 'Invalid API Key'}), 401
+    
+    if request.method == 'POST':
+        temp = query_db('update rooms set name = ? where id = ? returning id, name',
+            (request.args['name'], request.args['room_id']),
+            one=True
+        )
+        return {}, 200
+    return {'Status': 'Failed for Unknown Reasons'}, 403
 
 # GET to get all the messages in a room
+@app.route('/api/room/messages', methods=['GET'])
+def get_all_messages():
+    print(request.args)
+    print(request.headers)
+    out = {}
+    user = validate_user_api_key(request)
+    if not user:
+        return render_template('error.html', args={"trace: ": 'Invalid API Key'}), 401
+    if request.method == 'GET':
+        room_id = request.args['room_id']
+        msgs = query_db('select m.id, u.name, m.body from messages m, users u '
+                       'where m.room_id = ? and m.user_id = u.id order by m.id', [room_id], one=False)
+        if not msgs:
+            return out
+        for msg in msgs:
+            out[msg[0]] = {'id': msg[0], 'name': msg[1], 'body': msg[2]}
+    return out, 200
 
 # POST to post a new message to a room
+@app.route('/api/room/post', methods=['POST'])
+def post_message():
+    user = validate_user_api_key(request)
+    if not user:
+        return render_template('error.html', args={"trace: ": 'Invalid API Key'}), 401
+    if request.method == 'POST':
+        max_id = get_max_msg_id()
+        u = query_db('insert into messages (id, user_id, room_id, body) ' + 
+            'values (?, ?, ?, ?) returning id, user_id, room_id, body',
+            (int(max_id[0]) + 1, request.headers['User-Id'], request.args['room_id'], request.args['body']), one=True)
+        return {'status': 'Success'}, 200
--- static/script.js
+++ static/script.js
@@ -2,6 +2,8 @@
 
 // TODO: If a user clicks to create a chat, create an auth key for them
 // and save it. Redirect the user to /chat/<chat_id>
+const api_key = WATCH_PARTY_API_KEY
+const user_id = WATCH_PARTY_USER_ID
 function createChat() {
 
 }
@@ -11,14 +13,129 @@ function createChat() {
 // TODO: Fetch the list of existing chat messages.
 // POST to the API when the user posts a new message.
 // Automatically poll for new messages on a regular interval.
-function postMessage() {
+const ALL_MESSAGES_URL = '/api/room/messages'
+const POST_MESSAGE = '/api/room/post'
+const UPDATE_USERNAME = '/api/update/username'
+const UPDATE_PASSWORD = '/api/update/password'
+const UPDATE_ROOM = '/api/update/room'
+var prevNoMsgs = 0
+var max_id = 0
+
+let getAllMsgsRequest = {
+    room_id: 0,
+    maxId: 0
+};
+
+let postRequest = {
+  room_id: 0,
+  body: ''
+};
+
+let postUpdateNameRequest = {
+  user_name: ''
+};
+
+let postUpdatePassRequest = {
+  password: ''
+};
+
+let postUpdateRoomRequest = {
+  name: '',
+  room_id: 0
+};
+
+
+async function updateUsername(name) {
+  postUpdateNameRequest.user_name = name;
+  postMsg = await createUrl(UPDATE_USERNAME, postUpdateNameRequest, 'POST');
+  // document.cookie = "user_id=" + name;
   return;
 }
 
-function getMessages() {
+async function updatePassword(pass) {
+  postUpdatePassRequest.password = pass;
+  postMsg = await createUrl(UPDATE_PASSWORD, postUpdatePassRequest, 'POST')
+  // document.cookie = "user_password=" + pass;
   return;
 }
 
-function startMessagePolling() {
+function toggleEditMode() {
+  event.preventDefault();
+
+  document.querySelector('.display').classList.add('hide');
+  document.querySelector('.edit').classList.remove('hide');
+}
+
+async function saveRoomName(room_id, n) {
+  console.log(n);
+  postUpdateRoomRequest.name = n;
+  postUpdateRoomRequest.room_id = room_id;
+  var response = await createUrl(UPDATE_ROOM, postUpdateRoomRequest, 'POST');
+  document.querySelector('.roomName').textContent = n;
+  document.querySelector('.edit').classList.add('hide');
+  document.querySelector('.display').classList.remove('hide');
+}
+
+async function createUrl(endPoint, requestBody, endType){
+  let url = endPoint + "?" + Object.keys(requestBody).map((key) => key+"="+encodeURIComponent(requestBody[key])).join("&");
+
+  let urlHeaders = new Headers();
+  urlHeaders.append("Api-Key", api_key);
+  urlHeaders.append("Accept", "application/json");
+  urlHeaders.append("Content-Type", "application/json");
+  urlHeaders.append("User-Id", user_id);
+  if(endPoint == UPDATE_PASSWORD){
+    urlHeaders.append("Password", Object.keys(requestBody).map((key) => requestBody[key]).join(''));
+    url = endPoint;
+  }
+
+  const myInit = {
+    method: endType,
+    headers: urlHeaders,
+  };
+  data = await fetch(url, myInit);
+  jsonForm = await data.json();
+  return jsonForm
+}
+
+async function postMessage(room_id, body) {
+  console.log(body);
+  postRequest.room_id = room_id;
+  postRequest.body = body;
+  postMsg = await createUrl(POST_MESSAGE, postRequest, 'POST')
+  document.getElementById("commentForm").reset();
+  return;
+}
+
+async function getMessages(room_id) {
+  getAllMsgsRequest.room_id = room_id;
+  getAllMsgsRequest.maxId = max_id;
+  let messages = await createUrl(ALL_MESSAGES_URL, getAllMsgsRequest, 'GET')
+  let messagesDiv = document.body.querySelector(".messages");
+  let child = messagesDiv.lastElementChild;
+  while (child) {
+    messagesDiv.removeChild(child);
+    child = messagesDiv.lastElementChild;
+  }
+
+  Object.keys(messages).forEach(key => {
+    let message = document.createElement("message");
+    let author = document.createElement("author");
+    author.innerHTML = messages[key].name;
+    let content = document.createElement("content");
+    content.innerHTML = messages[key].body;
+    message.appendChild(author);
+    message.appendChild(content);
+    messagesDiv.append(message);
+  });
+  
+  max_id = messagesDiv.max_id;
+  return;
+}
+
+async function startMessagePolling(room_id) {
+  setInterval(async () => {
+    await getMessages(room_id);
+  }, 100);
   return;
 }
